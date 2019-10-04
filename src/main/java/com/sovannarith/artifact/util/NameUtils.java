50fee4843ef37d2b5cc06b971c1462392fed5497
Cheav Sovannarith <cheavsovannarith@gmail.com>
2019-10-04 15:57:51 +0700

    add class v 2

--- src/main/java/com/sovannarith/artifact/util/NameUtils.java
+++ src/main/java/com/sovannarith/artifact/util/NameUtils.java
@@ -5,4 +5,8 @@ public class NameUtils {
     public static void print(){
         System.out.println("My Name is Cheav Sovannarith");
     }
+
+    public static void printV2(){
+        System.out.println("My Name is Cheav Sovannarith Version 2");
+    }
 }
